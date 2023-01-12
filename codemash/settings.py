519c7940396d0572084fb6b81661f7059dc326b5
Mano Sriram <mano.sriram0@gmail.com>
2023-01-13 03:11:36 +0530

    add cors

--- codemash/settings.py
+++ codemash/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     #  'app',
     'app.apps.AppConfig',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8000',
 ]
 
 ROOT_URLCONF = 'codemash.urls'
